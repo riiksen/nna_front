da344c7d2e739cb0ba6ee6b6453778b072b0030c
ksewo <you@example.com>
2019-11-16 00:43:27 +0100

    Added lessRule, which does not work

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,17 @@
 module.exports = {
   webpack: function(config, env) {
+    
+    //TODO: add MiniCssExtractPlugin on production
+    const lessRule = {
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader'
+      ],
+    };
+
+    config.module.rules = config.module.rules.concat(lessRule);
 
     return config;
   }

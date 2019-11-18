15cbec8b6eb0573e61d81a588a2f613c1dba19ce
ksewo <you@example.com>
2019-11-18 19:59:51 +0100

    Added global variables

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,11 @@
 const { addLessLoader, override } = require('customize-cra');
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
+
+const lessVariables = lessToJs(fs.readFileSync('./variables.less', 'utf8'));
 
 module.exports = override(
-  addLessLoader(),
+  addLessLoader({
+    'modifyVars':  lessVariables,
+  }),
 );

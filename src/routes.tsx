5b87214ed3754cb87d7d63f01f88ef05bd4de0dc
ksewo <you@example.com>
2019-11-18 20:03:09 +0100

    Component does not return BrowserRouter for now, it is in App.tsx, so It is possible to use <Link> in every component

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import { Index } from './components/Page/Index/Index';
+import { Jackpot } from './components/Page/Jackpot/Jackpot';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path='/' exact component={Index} />
+      <Route path='/jackpot' component={Jackpot} />
+    </Switch>
+  );
+};
+
+export { Routes };

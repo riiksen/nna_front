539c4c4ca34958936dd64e433be4e3bdaa15922d
Mike <certexplay@gmail.com>
2019-11-19 04:17:53 +0100

    added accessibility modifier to render method of Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import './header.less';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class Header extends React.Component {
-  render(): React.ReactNode {
+  public render(): React.ReactNode {
     return (
       <header>
         <ul>

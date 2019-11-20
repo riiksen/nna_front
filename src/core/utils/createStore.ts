6375e8ae30b9c39605d74f049a04d91176267ef2
Mike <certexplay@gmail.com>
2019-11-20 23:14:22 +0100

    added redux-saga middleware to redux store

--- src/core/utils/createStore.ts
+++ src/core/utils/createStore.ts
@@ -1,8 +1,14 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 // TODO: Add path mappings
 import { reducers } from '../reducers';
+import { sagas } from '../sagas';
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+const enhancers = compose(applyMiddleware(sagaMiddleware));
+
+const store = createStore(reducers, enhancers);
+sagaMiddleware.run(sagas);
 
 export { store };

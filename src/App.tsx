5f344510668ab6f66f2dd52082613871ce6d5749
Mike <certexplay@gmail.com>
2019-12-06 17:53:46 +0100

    added ReduxProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 
+import { store } from './core/utils/createStore';
 import { Router } from './router';
 
 // eslint-disable-next-line arrow-body-style
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Router />
+      <ReduxProvider store={store}>
+        <Router />
+      </ReduxProvider>
     </div>
   );
 };

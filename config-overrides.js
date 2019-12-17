23ae8024da439d62f6fa4ec39659748e3e1eb763
ksewo <you@example.com>
2019-12-17 21:12:58 +0100

    added webpack alias (@ to /src), does not work

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,15 @@
-const { addLessLoader, override } = require('customize-cra');
+const { addWebpackAlias, addLessLoader, override } = require('customize-cra');
 const fs = require('fs');
 const lessToJs = require('less-vars-to-js');
+const path = require('path');
 
 const lessVariables = lessToJs(fs.readFileSync('./variables.less', 'utf8'));
 
 module.exports = override(
   addLessLoader({
     'modifyVars':  lessVariables,
+  }),
+  addWebpackAlias({
+    ['@']: path.resolve(__dirname, 'src'),
   }),
 );
